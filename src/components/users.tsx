0f04338e12884e18dee5659cbd06d0b138ff1119
Jayasriraam-s <jayasriraam27@gmail.com>
2024-07-29 18:08:03 +0530

    change margin bottom in users

--- src/components/users.tsx
+++ src/components/users.tsx
@@ -36,7 +36,7 @@ const UserTable: React.FC = () => {
 
   return (
     <>
-      <div className="relative overflow-x-auto  max-w-[1200px] mx-auto my-0  md:mt-[100px]">
+      <div className="relative overflow-x-auto  max-w-[1200px] mx-auto my-0  md:mt-[100px] mb-3">
         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
